2b4c48c24e0462c8f2a927da2f1f9a0b1807a0d2
AbdulSamman <sammanab@outlook.de>
2024-04-01 18:54:04 +0200

    finished

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import fs from "fs";
 import cors from "cors"
 
 const app=express()
-const port = 3668
+const port = 3778
 app.use(cors())
 
-const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json","utf8"))
+const jobs = JSON.parse(fs.readFileSync("./src/data/jobs.json","utf8")) as Job[];
 const skills=JSON.parse(fs.readFileSync("./src/data/skillInfo.json","utf8"))
+
+type Job={
+    id:number,title:string,company:string,url:string,description:string,skillList:string,publicationDate:string,todo:string
+}
 
 app.get("/",(req:express.Request,res:express.Response)=>{
 res.send("Jobs site API")
@@ -15,6 +19,17 @@ res.send("Jobs site API")
 
 app.get("/jobs",(req:express.Request,res:express.Response)=>{
     res.json((jobs))
+})
+// add todos
+app.get("/todos",(req:express.Request,res:express.Response)=>{
+    res.json(jobs.map((job:Job)=>{
+        return {
+            todo:job.todo,
+            company:job.company,
+            title:job.title,
+            test:"abdul"
+        }
+    }))
 })
 
 app.get("/skills",(req:express.Request,res:express.Response)=>{
